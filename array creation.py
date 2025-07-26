c6b2ca66fe100f844f1d45f0d79a919e789bae23
chidu2007-png <gptmh98@gmail.com>
2025-07-25 22:45:38 -0700

    Add files via upload

---
+++ array creation.py
@@ -0,0 +1,6 @@
+import numpy as np
+arr=np.array([[1,2,3],[4,2,5]])
+print("array is of type:",type(arr))
+print("No.of dimentons:",arr.ndim)
+print("shape of array:",arr.shape)
+print("array stores elements of type:",arr.dtype)
