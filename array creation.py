7f02dcd17b6cae1f8258add495db7863f6aea7ee
chidu2007-png <gptmh98@gmail.com>
2025-09-19 13:09:12 +0530

    Delete array creation.py

--- array creation.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-arr=np.array([[1,2,3],[4,2,5]])
-print("array is of type:",type(arr))
-print("No.of dimentons:",arr.ndim)
-print("shape of array:",arr.shape)
-print("array stores elements of type:",arr.dtype)
